64ddfcb5b601f3df0ab16f7e0a1f00048222debc
Aleksandr Spitsyn <aleksandr.spitsyn@xpoint.tech>
2023-10-11 12:39:19 +0400

    Initial commit

---
+++ src/main/java/com/asp/phonebooking/PhoneBookingApplication.java
@@ -0,0 +1,13 @@
+package com.asp.phonebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asp/phonebooking/PhoneBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asp.phonebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonebookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
