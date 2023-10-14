cc71f348d9bc3936105b96ff09dff45082313ff3
Aleksandr Spitsyn <aleksandr.spitsyn@xpoint.tech>
2023-10-14 13:55:04 +0400

    Fix error handling

--- src/main/java/com/asp/phonebooking/api/controller/ControllerAdvisor.java
+++ src/main/java/com/asp/phonebooking/api/controller/ControllerAdvisor.java
@@ -6,8 +6,11 @@ import com.asp.phonebooking.exception.InvalidBookingException;
 import com.asp.phonebooking.exception.NotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -34,6 +37,21 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
     @ExceptionHandler(InvalidBookingException.class)
     public ResponseEntity<Object> handleAlreadyExistsException(InvalidBookingException exc, WebRequest req) {
         return handleException(HttpStatus.BAD_REQUEST, exc, req);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exc,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest req) {
+        var errorResponseBuilder = ErrorResponse.builder(exc, HttpStatus.BAD_REQUEST, "Arguments are not valid");
+        exc.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errorResponseBuilder.property(fieldName, errorMessage);
+        });
+
+        return handleExceptionInternal(exc, errorResponseBuilder.build(), HttpHeaders.EMPTY, HttpStatus.BAD_REQUEST, req);
     }
 
     private ResponseEntity<Object> handleException(HttpStatus httpStatus, Exception exc, WebRequest req) {
